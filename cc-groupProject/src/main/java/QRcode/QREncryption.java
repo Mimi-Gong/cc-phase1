cb8f1b60c30384dc2cac8c72f2e80946bb997e60
Mimi-Gong <mimig@andrew.cmu.edu>
2018-10-16 16:52:12 -0700

    update

---
+++ cc-groupProject/src/main/java/QRcode/QREncryption.java
@@ -0,0 +1,12 @@
+package QRcode;
+
+public class QREncryption {
+    
+    public void QREncryption() {
+        return;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("QR project");
+    }
+}
---
+++ cc-groupProject/src/test/java/QRTest/QREncryptionTest.java
@@ -0,0 +1,5 @@
+package QRTest;
+
+public class QREncryptionTest {
+
+}
